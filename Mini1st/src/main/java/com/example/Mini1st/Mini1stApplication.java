7468de23fea8650216e72f7f486ef362d336d91f
mysk423 <80307373+mysk423@users.noreply.github.com>
2024-10-09 17:40:29 +0900

    10-09
    미니 프로젝트 기본 설정 추가
    - ignore 추가
    - 프로젝트 생성

---
+++ Mini1st/src/main/java/com/example/Mini1st/Mini1stApplication.java
@@ -0,0 +1,13 @@
+package com.example.Mini1st;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Mini1stApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Mini1stApplication.class, args);
+	}
+
+}
