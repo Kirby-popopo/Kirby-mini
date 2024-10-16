e1d557df80ddc07aa7efed8614c2bac057b8896b
mysk423 <80307373+mysk423@users.noreply.github.com>
2024-10-16 11:41:06 +0900

    10 - 16 - css 수정
    - 검색시 a 태그 내부 코드 색상 변경

--- Mini1st/src/main/resources/static/sidebar/menu.js
+++ Mini1st/src/main/resources/static/sidebar/menu.js
@@ -33,7 +33,12 @@
 
                     search_section.innerHTML = "";
                         users.forEach(user => {
-                            search_section.innerHTML += `<a href="http://localhost:8080/${user.name}"><div class="search-item"><img src="/placeholder.svg?height=30&width=30" alt="User"><span>${user.name}</span></div></a>`;
+                            search_section.innerHTML += `<a href="http://localhost:8090/${user.name}">
+                                                            <div class="search-item">
+                                                                <img src="/placeholder.svg?height=30&width=30" alt="User">
+                                                                <span>${user.name}</span>
+                                                            </div>
+                                                         </a>`;
                         });
        			    }
        		};
